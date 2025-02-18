8201c132b6a3b86c8c8fc387e7a4916057bd6df7
biancavitoria15 <bianca1.vitoria@gmail.com>
2025-02-18 12:19:26 -0300

    DTO

--- src/main/java/br/com/fiap/api_rest_aula01/controller/ClienteController.java
+++ src/main/java/br/com/fiap/api_rest_aula01/controller/ClienteController.java
@@ -7,38 +7,40 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
     @Autowired
     ClienteRepository clienteRepository;
+    @Autowired
+    ClienteService clienteService;
 
     // Create, Read, Update, Delete
     // Post, Get, Put, Delete
 
     @PostMapping
-    public ResponseEntity<Cliente> createCliente(@RequestBody Cliente cliente) {
-        Cliente clienteSalvo = clienteRepository.save(cliente);
+    public ResponseEntity<Cliente> createCliente(@RequestBody ClienteRequest cliente) {
+        Cliente clienteSalvo = clienteRepository.save(clienteService.requestToCliente(cliente));
         return new ResponseEntity<>(clienteSalvo, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<Cliente>> readClientes() {
+    public ResponseEntity<List<ClienteResponse>> readClientes() {
         List<Cliente> clientes = clienteRepository.findAll();
-        return new ResponseEntity<>(clientes, HttpStatus.OK);
+        return new ResponseEntity<>(clienteService.clientesToResponse(clientes), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Cliente> readCliente(@PathVariable Long id) {
+    public ResponseEntity<ClienteResponse> readCliente(@PathVariable Long id) {
         Optional<Cliente> cliente = clienteRepository.findById(id);
         if (cliente.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(cliente.get(), HttpStatus.OK);
+        return new ResponseEntity<>(clienteService.clienteToResponse(cliente.get()), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
---
+++ src/main/java/br/com/fiap/api_rest_aula01/dto/ClienteRequest.java
@@ -0,0 +1,22 @@
+package br.com.fiap.api_rest_aula01.dto;
+
+puclic class ClienteRequest{
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
